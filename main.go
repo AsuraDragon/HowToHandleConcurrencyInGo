2ac225409afb518c6a5fbc732dbc76177b877998
Héctor Manuel Rasura Fragozo <rasurafragozo_@hotmail.com>
2025-07-14 04:12:17 -0700

    Adding wait groups to wait for goroutines

--- main.go
+++ main.go
@@ -3,27 +3,34 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
+
+var wg sync.WaitGroup
 
 func pause() {
 	time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
 }
 
 func sendMsg(msg string) {
+	defer wg.Done()
 	pause()
 	fmt.Println(msg)
 }
 
 func main() {
-	sendMsg("Hello") //sync
+	wg.Add(4)
+	defer wg.Wait()
+	go func(msg string) {
+		defer wg.Done()
+		pause()
+		fmt.Println(msg)
+	}("Ella")
 
-	go sendMsg("Ella") //Async
-	go sendMsg("No")   //Async
-	go sendMsg("Te")   //Async
-	go sendMsg("Ama")  //Async
-
-	sendMsg("World!") //sync
+	go sendMsg("No")  //Async
+	go sendMsg("Te")  //Async
+	go sendMsg("Ama") //Async
 
 	time.Sleep(2 * time.Second)
 
